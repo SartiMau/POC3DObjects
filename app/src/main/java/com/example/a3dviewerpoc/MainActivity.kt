a69761b4bfa6127de5537416d613b6c50d83dccc
Mauro Sarti <mauro.sarti@globant.com>
2021-12-29 13:26:23 -0300

    Our file working fine
    Tested in: Samsung S10+
    Signed off by: Mauro Sarti mauro.sarti@globant.com

--- app/src/main/java/com/example/a3dviewerpoc/MainActivity.kt
+++ app/src/main/java/com/example/a3dviewerpoc/MainActivity.kt
@@ -1,35 +1,27 @@
 package com.example.a3dviewerpoc
 
-import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val requiredPermissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-        ActivityCompat.requestPermissions(this, requiredPermissions, 0)
-
-        val downloadDir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
-
         val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
         val intentUri = Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
             .appendQueryParameter(
                 "file",
-//                "$downloadDir/house_obj.gltf"
-                "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"
+                "https://raw.githubusercontent.com/SartiMau/POC3DObjects/main/house_obj.gltf"
+//                "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"
             )
             .appendQueryParameter("mode", "3d_only")
             .build()
         sceneViewerIntent.data = intentUri
-//        sceneViewerIntent.setPackage("com.google.ar.core") // AR-Core app
-        sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox") // Google app
+        sceneViewerIntent.setPackage("com.google.ar.core") // AR-Core app
+//        sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox") // Google app
         startActivity(sceneViewerIntent)
     }
 }
