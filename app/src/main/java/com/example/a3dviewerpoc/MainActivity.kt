7369a7cac5a9794236159dcbbdca57a7f0897422
Mauro Sarti <mauro.sarti@globant.com>
2021-12-29 12:57:00 -0300

    Example working
    Tested in: Samsung S10+
    Signed off by: Mauro Sarti mauro.sarti@globant.com

--- app/src/main/java/com/example/a3dviewerpoc/MainActivity.kt
+++ app/src/main/java/com/example/a3dviewerpoc/MainActivity.kt
@@ -1,46 +1,34 @@
 package com.example.a3dviewerpoc
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
+import android.os.Bundle
 import android.os.Environment
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val url = "www.google.com"
+        val requiredPermissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(this, requiredPermissions, 0)
 
-        var webpage = Uri.parse(url)
-
-        if (!url.startsWith("http://") && !url.startsWith("https://")) {
-            webpage = Uri.parse("http://$url")
-        }
-
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
-
-        /*
-        val pepe = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        val downloadDir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
 
         val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
-        val intentUri = Uri.parse("http://arvr.google.com/scene-viewer/1.0").buildUpon()
+        val intentUri = Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
+            .appendQueryParameter(
+                "file",
+//                "$downloadDir/house_obj.gltf"
+                "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"
+            )
             .appendQueryParameter("mode", "3d_only")
             .build()
         sceneViewerIntent.data = intentUri
         sceneViewerIntent.setPackage("com.google.ar.core")
         startActivity(sceneViewerIntent)
-        */
-        /*
-        val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
-        sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=$pepe/house_obj.obj"));
-        sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
-        startActivity(sceneViewerIntent)
-        */
     }
 }
